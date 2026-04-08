4902b0dbdce134262a95c13139ea4629150042c5
Katehok <a.timin2003@mail.ru>
2026-04-08 22:29:00 +0300

    refactor: extract refresh token creation and insertion to TokenService.set_refresh_token

--- app/src/services.py
+++ app/src/services.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timezone
 from fastapi import HTTPException, status
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.config import settings
 from src.crypt import Password, JWT, SymmetricEncryption, Random
@@ -38,16 +39,9 @@ class RegisterService:
         key = key_part + ":" + pin_code
         key_cipher = SymmetricEncryption.encrypt(key.encode('utf-8'))
         user = await AsyncAuthRepo.add_user(hashed_password, key_cipher, session)
-        refresh_token, refresh_token_exp = JWT.issue_refresh_token(fast=True)
-        await AsyncAuthRepo.add_refresh_token(
-            user.id,
-            refresh_token,
-            False,
-            refresh_token_exp,
-            session
-        )
+        new_token = await TokenService.set_refresh_token(user.id, False, session)
         await session.commit()
-        return RegisterSchemas.Init.Resp(refresh_token=refresh_token, key_part=key_part)
+        return RegisterSchemas.Init.Resp(refresh_token=new_token.token, key_part=key_part)
 
     @staticmethod
     async def accept(refresh_token: str, session: AsyncSession) -> RegisterSchemas.Accept.Resp:
@@ -59,15 +53,10 @@ class RegisterService:
         user = await AsyncAuthRepo.select_user(old_token.user_id, session)
         if user is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        if user.is_active:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exist")
         user.is_active = True
-        refresh_token, refresh_token_exp = JWT.issue_refresh_token()
-        new_token = await AsyncAuthRepo.add_refresh_token(
-            user.id,
-            refresh_token,
-            True,
-            refresh_token_exp,
-            session
-        )
+        new_token = await TokenService.set_refresh_token(user.id, True, session)
         await session.commit()
         return RegisterSchemas.Accept.Resp(
             refresh_token=new_token.token,
@@ -89,8 +78,24 @@ class TokenService:
     """Бизнес-логика работы с токенами"""
 
     @staticmethod
-    async def get_refresh_token(refresh_token: str, accepted: bool, session: AsyncSession) -> RefreshTokenModel:
-        """Получает токен из БД (выбрасывает HTTP-исключения, accepted = True - требует подтвержденный токен)"""
+    async def set_refresh_token(user_id: int, accepted: bool, session: AsyncSession) -> RefreshTokenModel:
+        """Выпускает и добавляет в БД новый refresh-токен, обрабатывая нарушение уникальности (не выполняет коммит)"""
+        refresh_token, refresh_token_exp = JWT.issue_refresh_token()
+        try:
+            new_token = await AsyncAuthRepo.add_refresh_token(
+                user_id,
+                refresh_token,
+                accepted,
+                refresh_token_exp,
+                session
+            )
+            return new_token
+        except IntegrityError:
+            raise HTTPException(status_code=500, detail="Impossible! Failed to generate unique refresh token")
+
+    @staticmethod
+    async def get_refresh_token(refresh_token: str, require_accepted: bool, session: AsyncSession) -> RefreshTokenModel:
+        """Получает токен из БД (выбрасывает HTTP-исключения, require_accepted требует подтвержденный токен)"""
         token = await AsyncAuthRepo.select_refresh_token(refresh_token, session)
         if token is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
@@ -98,7 +103,7 @@ class TokenService:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
         if token.revoked:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token revoked")
-        if accepted and not token.accepted:
+        if require_accepted and not token.accepted:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token not accepted")
         return token
 
